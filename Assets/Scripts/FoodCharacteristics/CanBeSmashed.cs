8c339429405a4f96b9375fd098ba7b74bcbc0525
LAPTOP-O1078A81\fatmaasal <fasal@etu.edu.tr>
2018-01-15 23:33:26 +0300

    CanBeSmashed and CanSmash scripts are added.

---
+++ Assets/Scripts/FoodCharacteristics/CanBeSmashed.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanBeSmashed : MonoBehaviour {
+    public GameObject potato;
+    public GameObject mashedPotato;
+
+    void Start()
+    {
+        potato.SetActive(true);
+        mashedPotato.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider canSmash)
+    {
+        if (canSmash.gameObject.CompareTag("Smashable"))
+        {
+            potato.SetActive(false);
+            mashedPotato.transform.position = potato.transform.position;
+            mashedPotato.transform.rotation = potato.transform.rotation;
+            mashedPotato.SetActive(true);
+        }
+    }
+    
+}
