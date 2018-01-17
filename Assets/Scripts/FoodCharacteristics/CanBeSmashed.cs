5370fec94ef3f90a4b0dd62f75809304c2071396
Fatma ASAL <fasal@etu.edu.tr>
2018-01-17 16:26:28 +0300

    Smashing scene tested and bugs fixed.

--- Assets/Scripts/FoodCharacteristics/CanBeSmashed.cs
+++ Assets/Scripts/FoodCharacteristics/CanBeSmashed.cs
@@ -5,22 +5,43 @@ using UnityEngine;
 public class CanBeSmashed : MonoBehaviour {
     public GameObject potato;
     public GameObject mashedPotato;
+    public int smashCount;
 
     void Start()
     {
         potato.SetActive(true);
         mashedPotato.SetActive(false);
+        smashCount = 0;
     }
 
     void OnTriggerEnter(Collider canSmash)
     {
         if (canSmash.gameObject.CompareTag("Smashable"))
         {
+            Debug.Log("smashed");
             potato.SetActive(false);
             mashedPotato.transform.position = potato.transform.position;
             mashedPotato.transform.rotation = potato.transform.rotation;
             mashedPotato.SetActive(true);
         }
     }
-    
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "silicone_spatula")
+        {
+            if (smashCount >= 5)
+            {
+                potato.SetActive(false);
+                mashedPotato.transform.position = potato.transform.position;
+                mashedPotato.transform.rotation = potato.transform.rotation;
+                mashedPotato.SetActive(true);
+            }
+            else
+            {
+                smashCount = smashCount + 1;
+            }
+            
+        }
+    }
+
 }
