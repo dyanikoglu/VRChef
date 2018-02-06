0311aa74167781dd76a07c9156535d5ea9669436
Fatma ASAL <fasal@etu.edu.tr>
2018-02-06 23:31:22 +0300

    For putting ingredients like flour, sugar  in a bowl scripts are added and scene is developed. Models and prefabs are added.

---
+++ Assets/Scripts/ToolCharacteristics/CanBeTakenFrom.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanBeTakenFrom : MonoBehaviour {
+
+
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<CanTake>() != null)
+        {
+            other.gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            CanTake.full = true;
+            Vector3 pos = gameObject.transform.position;
+            pos.y = pos.y - (float)0.015;
+            gameObject.transform.position = pos;
+        }
+    }
+}
---
+++ Assets/Scripts/ToolCharacteristics/CanTake.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class CanTake: ToolCharacteristic {
+
+    // Use this for initialization
+    ParticleSystem dust;
+    public static bool full = false;
+	void Start () {
+        gameObject.transform.GetChild(0).gameObject.SetActive(false);
+        dust = GetComponentInChildren<ParticleSystem>();
+    }
+    GameObject poured;
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown("space") && GetIsGrabbed() && full)
+        {
+            Vector3 pos = dust.transform.position;
+            pos.y = gameObject.transform.position.y;
+            pos.x = gameObject.transform.position.x;
+            dust.transform.position = pos; 
+            dust.Play(); 
+            gameObject.transform.GetChild(0).gameObject.SetActive(false);
+            full = false;
+        }
+
+        
+	}
+}
