68a942e1de7d3688fb4162004ffe63d58d6e7298
Ertuğrul Güngör <ertugungor@gmail.com>
2018-02-08 22:02:18 +0300

    Example scene for testing frying process has been constructed. To do this, a script, a shader and some textures have been added.

---
+++ Assets/CanBeFried.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanBeFried : MonoBehaviour
+{
+
+    [SerializeField]
+    Texture objectTexture;
+    Material myMaterial;
+
+    public Shader shaderWithBlending;
+    public Texture friedTexture;
+
+    // Use this for initialization
+    void Start()
+    {
+        // get object's material.
+        myMaterial = GetComponent<Renderer>().material;
+        // if object has a texture assign it to objectTexture, otherwise create a texture from its color property
+        objectTexture = myMaterial.mainTexture;
+        if (!objectTexture)
+        {
+            Texture2D texture = new Texture2D(2, 2, TextureFormat.ARGB32, false);
+
+            // set pixel values
+            Color color = myMaterial.color;
+            texture.SetPixel(0, 0, color);
+            texture.SetPixel(1, 0, color);
+            texture.SetPixel(0, 1, color);
+            texture.SetPixel(1, 1, color);
+
+            // apply pixels
+            texture.Apply();
+            objectTexture = texture;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myMaterial.shader = shaderWithBlending;
+            myMaterial.SetTexture("_MainTex", objectTexture);
+            //myMaterial.SetTexture("_SecondTex", friedTexture);
+
+            myMaterial.SetTexture("_Texture2", friedTexture);
+
+            //myMaterial.SetTexture("_TexMat1", objectTexture);
+            //myMaterial.SetTexture("_TexMat2", friedTexture);
+
+            //myMaterial.SetTexture("_TerrainFloor", objectTexture);
+            //myMaterial.SetTexture("_Sand", friedTexture);
+
+        }
+
+    }
+}
---
+++ Assets/Shaders/TextureBlendingShader.shader
@@ -0,0 +1,39 @@
+﻿Shader "Diffuse Bump Blending"
+{
+  Properties
+  {
+      _Blend ("Blend", Range(0, 1)) = 0.5 
+      _MainTex ("Texture", 2D) = ""
+      _Texture2 ("Texture 2", 2D) = ""
+      _BumpMap ("Bumpmap", 2D) = "bump" {}
+  }
+
+
+  SubShader
+  {
+    Tags { "RenderType" = "Opaque" }
+    CGPROGRAM
+    #pragma surface surf Lambert
+    struct Input
+    {
+      float2 uv_MainTex;
+      float2 uv_BumpMap;
+    };
+
+
+ sampler2D _MainTex;
+ sampler2D _Texture2;
+ sampler2D _BumpMap;
+ float _Blend;
+ void surf (Input IN, inout SurfaceOutput o)
+ {
+   float3 t1 = tex2D (_MainTex, IN.uv_MainTex).rgb;
+   float3 t2 = tex2D (_Texture2, IN.uv_MainTex).rgb;
+   o.Albedo = lerp(t1, t2, _Blend);
+   o.Normal = UnpackNormal (tex2D (_BumpMap, IN.uv_BumpMap));
+ }
+ ENDCG
+
+  } 
+  Fallback "Diffuse"
+}
