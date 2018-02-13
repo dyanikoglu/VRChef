f161ec7c4228539b3e0bc09b94217bb7684c3e29
Ertuğrul Güngör <ertugungor@gmail.com>
2018-02-13 22:31:56 +0300

    Frying process with texture blending has been implemented

--- Assets/CanBeFried.cs
+++ Assets/CanBeFried.cs
@@ -12,25 +12,26 @@ public class CanBeFried : MonoBehaviour
     public Shader shaderWithBlending;
     public Texture friedTexture;
 
+    public float fryingTimeInSeconds;
+
     // Use this for initialization
     void Start()
     {
-        // get object's material.
+
         myMaterial = GetComponent<Renderer>().material;
-        // if object has a texture assign it to objectTexture, otherwise create a texture from its color property
         objectTexture = myMaterial.mainTexture;
         if (!objectTexture)
         {
             Texture2D texture = new Texture2D(2, 2, TextureFormat.ARGB32, false);
 
-            // set pixel values
+            // set the pixel values
             Color color = myMaterial.color;
             texture.SetPixel(0, 0, color);
             texture.SetPixel(1, 0, color);
             texture.SetPixel(0, 1, color);
             texture.SetPixel(1, 1, color);
 
-            // apply pixels
+            // Apply all SetPixel calls
             texture.Apply();
             objectTexture = texture;
         }
@@ -41,19 +42,36 @@ public class CanBeFried : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            // replace shader with one can blend textures
             myMaterial.shader = shaderWithBlending;
-            myMaterial.SetTexture("_MainTex", objectTexture);
-            //myMaterial.SetTexture("_SecondTex", friedTexture);
 
+            // set textures of object. (One for initial state, one for fried state)
+            myMaterial.SetTexture("_MainTex", objectTexture);
             myMaterial.SetTexture("_Texture2", friedTexture);
 
-            //myMaterial.SetTexture("_TexMat1", objectTexture);
-            //myMaterial.SetTexture("_TexMat2", friedTexture);
-
-            //myMaterial.SetTexture("_TerrainFloor", objectTexture);
-            //myMaterial.SetTexture("_Sand", friedTexture);
-
+            // to animate object's frying process
+            StartCoroutine("Fry");
         }
 
     }
+
+    IEnumerator Fry()
+    {
+        /* <Method Summary>
+         * Method animates frying process by changing texture blending relative to 
+         * frying time. Once frying time passed, blend value will be 1 implying
+         * object's texture include only it's fried texture
+         * <Method Summary>
+         */
+
+        float blend = myMaterial.GetFloat("_Blend");
+        while (blend < 1f)
+        {
+            myMaterial.SetFloat("_Blend", blend);
+            float fadeValue = Time.deltaTime / fryingTimeInSeconds;
+            blend += fadeValue;
+            yield return new WaitForSeconds(fadeValue);
+        }
+    }
+
 }
