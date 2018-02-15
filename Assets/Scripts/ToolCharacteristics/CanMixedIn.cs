f8b2d1365bb11a0ac4163fed71ae786d20d9e8d4
Fatma ASAL <fasal@etu.edu.tr>
2018-02-16 00:39:29 +0300

    Bugs in fluid behavior in mixing fixed.

--- Assets/Scripts/ToolCharacteristics/CanMixedIn.cs
+++ Assets/Scripts/ToolCharacteristics/CanMixedIn.cs
@@ -7,11 +7,14 @@ public class CanMixedIn : ToolCharacteristic {
     private bool collided;
     public Obi.ObiEmitter emitter;
     private int rotateCount;
-    public GameObject finalObject;
+
     // Use this for initialization
     void Start () {
         collided=false;
         rotateCount=0;
+        ObiEmitterMaterialFluid material = (ObiEmitterMaterialFluid)emitter.gameObject.GetComponent<ObiEmitter>().EmitterMaterial;
+        material.atmosphericPressure = (float)0;
+        emitter.Solver.UpdateActiveParticles();
     }
 	
 	// Update is called once per frame
@@ -33,20 +36,36 @@ public class CanMixedIn : ToolCharacteristic {
     {
         if (other.gameObject.GetComponent<CanMix>() != null)
         {
-            for (int i=1; i < gameObject.transform.childCount; i++)
+            for (int i=2; i < gameObject.transform.childCount; i++)
             {
                 gameObject.transform.GetChild(i).transform.Rotate(Vector3.up * 400 * Time.deltaTime, Space.World); 
             }
+            ObiEmitterMaterialFluid material = (ObiEmitterMaterialFluid)emitter.gameObject.GetComponent<ObiEmitter>().EmitterMaterial;
+            material.atmosphericPressure = (float)10;
+            emitter.Solver.UpdateActiveParticles();
             rotateCount++;
             if (rotateCount > 200)
             {
-                GameObject newObject = Instantiate(finalObject, transform.position, finalObject.transform.rotation);
+                gameObject.transform.GetChild(1).gameObject.transform.position = gameObject.transform.position;
+                gameObject.transform.GetChild(1).gameObject.SetActive(true);
                 emitter.enabled = false;
-                for (int i = 1; i < gameObject.transform.childCount; i++)
+                for (int i = 2; i < gameObject.transform.childCount; i++)
                 {
                     Destroy(gameObject.transform.GetChild(i).gameObject);
+                    material.atmosphericPressure = (float)0;
+                    emitter.Solver.UpdateActiveParticles();
                 }
             }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.GetComponent<CanMix>() != null)
+        {
+            ObiEmitterMaterialFluid material = (ObiEmitterMaterialFluid)emitter.gameObject.GetComponent<ObiEmitter>().EmitterMaterial;
+            material.atmosphericPressure = (float)0;
+            emitter.Solver.UpdateActiveParticles();
         }
     }
 
