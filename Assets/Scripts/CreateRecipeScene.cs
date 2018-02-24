1b419207ee63c77d69dd245a0ffe60e2298c5daa
Fatma ASAL <fasal@etu.edu.tr>
2018-02-24 12:50:28 +0300

    For placing ingredients in certain places in scene before recipe started a training scene and script is created.

---
+++ Assets/Scripts/CreateRecipeScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateRecipeScene : MonoBehaviour {
+    public GameObject[] places;
+    public GameObject[] ingredients;
+    // Use this for initialization
+    void Start() {
+        //getTheIngredients();
+        //for each ingredient call the method for placing the ingredients.
+        Instantiate(ingredients[0], places[0].transform.position, ingredients[0].transform.rotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
