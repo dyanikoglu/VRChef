18945476a4541a1284beebe7472938755e5a5af3
Seçil Usta <secil095@gmail.com>
2018-02-24 18:06:22 +0300

    Script and prefabs are added for boiling.

---
+++ Assets/CanBoil.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanBoil : MonoBehaviour {
+
+    public bool canBoil = false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!canBoil && collision.transform.parent)
+        {
+            Debug.Log("enter " + collision.collider.name);
+            canBoil = OnOven(collision.collider.transform.parent.gameObject, collision.collider.transform.parent.transform.parent);
+        }
+    }
+
+    bool OnOven(GameObject plate, Transform parent)
+    {
+        bool isPlate = false;
+        if (parent != null)
+        {
+            if (parent.GetComponent<HotPlateOven>())
+            {
+                HotPlateOven hotPlateOven = parent.GetComponent<HotPlateOven>();
+                for (int i = 0; i < hotPlateOven.plates.Length; i++)
+                {
+                    if (hotPlateOven.plates[i].name == plate.name && hotPlateOven.activePlateIndices[i])
+                    {
+                        isPlate = true;
+                        return isPlate;
+                    }
+                }
+
+            }
+        }
+        Debug.Log("onOven: " + isPlate);
+        return isPlate;
+    }
+}
