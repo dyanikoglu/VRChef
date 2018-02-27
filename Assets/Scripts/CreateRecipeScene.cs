b0d21b40e9b48813886e0b8b97b01bd6f63c289a
Fatma ASAL <fasal@etu.edu.tr>
2018-02-27 23:04:15 +0300

    For mixing action scene is developed and for preparing ingredients in the kitchen script and scene is developed.

--- Assets/Scripts/CreateRecipeScene.cs
+++ Assets/Scripts/CreateRecipeScene.cs
@@ -2,31 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CreateRecipeScene : MonoBehaviour {
-    public GameObject[] places;
-    public GameObject[] ingredients;
-    // Use this for initialization
-    void Start() {
-        //getTheIngredients();
-        int i = 0;
-        for(int j = 0; j < ingredients.Length; j++)
+namespace RecipeModule { 
+    public class CreateRecipeScene : MonoBehaviour {
+        public GameObject[] places;
+        private bool isGetActionList = false;
+        int placeCount = 0;
+       // Use this for initialization
+        void Update()
         {
-            CreateObjectInScene(ingredients[j], places[i], 2);
-            i++;
-            if(i > places.Length)
+            if (!isGetActionList)
             {
-                i = 0;
+                isGetActionList = true;
+                UseActionList(gameObject.GetComponent<Recipe>().GetActions());
             }
-        } 
-	}
-
-    void CreateObjectInScene(GameObject ingredient, GameObject place, int quantity)
-    {
-        for (int i = 0; i < quantity; i++)
-        {
-            Instantiate(ingredient, place.transform.position, ingredient.transform.rotation);
         }
+
+        void CreateObjectInScene(GameObject ingredient, int quantity)
+        {
+            for (int i = 0; i < quantity; i++)
+            {
+                Instantiate(ingredient, places[placeCount].transform.position, ingredient.transform.rotation);
+            }
+            placeCount++;
+        }
+
+        void UseActionList(List<Action> actionList)
+        {
+            List<GameObject> f=new List<GameObject>();
+            foreach(Action a in actionList)
+            {
+                if (a.GetInvolvedFood().GetPrev() == null)
+                {
+                    if (f.Count==0)
+                    {
+                        f.Add(a.GetInvolvedFood().GetPrefab());
+                    }
+                    else if (f.Contains(a.GetInvolvedFood().GetPrefab()))
+                    {
+                        f.Add(a.GetInvolvedFood().GetPrefab());
+                    }
+                    else
+                    {
+                        CreateObjectInScene(f[0], f.Count);
+                        f.Clear();
+                        f.Add(a.GetInvolvedFood().GetPrefab());
+                    }
+                }
+            }
+            if (f != null)
+            {
+                CreateObjectInScene(f[0], f.Count);
+                f.Clear();
+            }
+        }
+
     }
-
-
 }
