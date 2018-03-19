66fee7161b18118fb1dee5f0589e4b9e8f9f947f
Fatma ASAL <fasal@etu.edu.tr>
2018-03-19 15:47:41 +0300

    For showing completed steps in ui recipe task list ui is added.

---
+++ Assets/Scripts/UI/AddActionsTaskList.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace RecipeModule
+{
+    public class AddActionsTaskList : MonoBehaviour
+    {
+        public GameObject taskUI;
+        public string recipe;
+        // Use this for initialization
+        void Start()
+        {
+            Recipe r = Recipe.LoadRecipe(recipe);
+            List<Action> actions = r.GetActions();
+            GameObject a;
+            
+            foreach( Action action in actions)
+            {
+                Vector3 pos = taskUI.transform.position;
+                pos.y = pos.y - 20;
+                taskUI.transform.position = pos;
+                a = Instantiate(taskUI,taskUI.transform.position,taskUI.transform.rotation);
+                a.transform.GetChild(0).gameObject.SetActive(false);
+                a.transform.SetParent(gameObject.transform);
+                a.GetComponent<Text>().text = action.GetActionType() + " "+ action.GetInvolvedFood().GetFoodIdentifier();
+            }
+            Vector3 pos1 = taskUI.transform.position;
+            pos1.y = 360;
+            taskUI.transform.position = pos1;
+        }
+
+        public void SetStepCompleted(int stepNumber)
+        {
+            print(stepNumber);
+            gameObject.transform.GetChild(stepNumber + 1).gameObject.transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+}
